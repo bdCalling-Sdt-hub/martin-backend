e94056d371f3a434e631b07caabcb5430024b0ef
Shehabuddin-Tushar <freelancerrtushar@gmail.com>
2024-05-25 16:05:57 +0600

    field serial number sorting complete

--- controllers/form.controller.js
+++ controllers/form.controller.js
@@ -8,136 +8,165 @@ const catchAsync = require("../shared/CatchAsync.js");
 const fs = require("fs");
 const path = require("path");
 
-exports.formFieldCreate=catchAsync(async (req, res, next) => {
-
+exports.formFieldCreate = catchAsync(async (req, res, next) => {
     const user = await User.findById(req.user._id);
-    if(user.role=="ADMIN" || "SUPER ADMIN"){
-        
-      const{title,dataType,optiondata}=req.body
-      console.log(title,dataType,optiondata)
-
-
+    if (user.role == "ADMIN" || "SUPER ADMIN") {
+      const { title, dataType, optiondata, fieldNumber } = req.body;
+      console.log(title, dataType, optiondata);
+  
       const form = await Form.create({
-        
-          title,
-          dataType,
-          optiondata
-         
+        title,
+        dataType,
+        optiondata,
+        fieldNumber,
       });
-
+  
       return sendResponse(res, {
         statusCode: httpStatus.OK,
         success: true,
         message: "Form field add successfully",
-        data:form
-    });
-
-
-    }else{
-        throw new ApiError(401, "You are unauthorized");
+        data: form,
+      });
+    } else {
+      throw new ApiError(401, "You are unauthorized");
     }
-
-});
-
-exports.showField=catchAsync(async (req, res, next) => {
-
-
+  });
+  
+  exports.showField = catchAsync(async (req, res, next) => {
     const user = await User.findById(req.user._id);
-    if(user.role=="ADMIN" || "SUPER ADMIN"){
-
-        let allFieldFetch=await Form.find();
-        console.log(allFieldFetch);
-
+    if (user.role == "ADMIN" || "SUPER ADMIN") {
+      let allFieldFetch = await Form.find().sort({ fieldNumber: "asc" });
+  
       return sendResponse(res, {
         statusCode: httpStatus.OK,
         success: true,
         message: "Form field fetch successfully",
-        data:allFieldFetch
-    });
-
-
-    }else{
-        throw new ApiError(401, "You are unauthorized");
+        data: allFieldFetch,
+      });
+    } else {
+      throw new ApiError(401, "You are unauthorized");
     }
-
-      
-});
-
-exports.fieldDelete=catchAsync(async (req, res, next) => {
-
-
+  });
+  
+  exports.getFieldNumber = catchAsync(async (req, res, next) => {
     const user = await User.findById(req.user._id);
-    if(user.role=="ADMIN" || "SUPER ADMIN"){
-
-        let Field=await Form.findByIdAndDelete(req.params.id);
- 
-
+    if (user.role == "ADMIN" || "SUPER ADMIN") {
+      let total = await Form.countDocuments();
+  
       return sendResponse(res, {
         statusCode: httpStatus.OK,
         success: true,
-        message: "Field Deleted successfully",
-        data:Field
-    });
+        message: "Total field count retrieved successfully",
+        data: total,
+      });
+    } else {
+      throw new ApiError(401, "You are unauthorized");
+    }
+  });
+
+  
+exports.fieldDelete = catchAsync(async (req, res, next) => {
 
 
-    }else{
+    const user = await User.findById(req.user._id);
+    if (user.role == "ADMIN" || "SUPER ADMIN") {
+
+        let Field = await Form.findByIdAndDelete(req.params.id);
+
+
+        return sendResponse(res, {
+            statusCode: httpStatus.OK,
+            success: true,
+            message: "Field Deleted successfully",
+            data: Field
+        });
+
+
+    } else {
         throw new ApiError(401, "You are unauthorized");
     }
 
-      
+
 });
 
 
-exports.pdfSave=catchAsync(async (req, res, next) => {
+exports.pdfSave = catchAsync(async (req, res, next) => {
     const user = await User.findById(req.user._id);
-    if(user.role=="ADMIN" || "SUPER ADMIN"){
-    
-       let pdfUrl ="";
-    
+    if (user.role == "ADMIN" || "SUPER ADMIN") {
 
-    if (req.files && req.files.pdfFile) {
-    
-            pdfUrl = `public/uploads/pdfs/${ req.files.pdfFile[0].filename}`;
+        let pdfUrl = "";
+
+
+        if (req.files && req.files.pdfFile) {
+
+            pdfUrl = `public/uploads/pdfs/${req.files.pdfFile[0].filename}`;
             //const publicFileUrl = createFileDetails('kyc', file.filename)
-          
-    } 
-    console.log(pdfUrl)
 
-    const Pdffile=await PdfFile.create({
-         pdf:pdfUrl
-    });
+        }
+        console.log(pdfUrl)
 
-    return sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Pdf file Upload successfully",
-        data:Pdffile
-    });
+        const Pdffile = await PdfFile.create({
+            pdf: pdfUrl
+        });
 
-  }else{
+        return sendResponse(res, {
+            statusCode: httpStatus.OK,
+            success: true,
+            message: "Pdf file Upload successfully",
+            data: Pdffile
+        });
+
+    } else {
         throw new ApiError(401, "You are unauthorized");
     }
 });
 
 
-exports.allPdfFetch=catchAsync(async (req, res, next) => {
+exports.allPdfFetch = catchAsync(async (req, res, next) => {
 
     const user = await User.findById(req.user._id);
-    if(user.role=="ADMIN" || "SUPER ADMIN"){
-        const Pdffile=await PdfFile.find();
-        if(Pdffile){
+    if (user.role == "ADMIN" || "SUPER ADMIN") {
+        const Pdffile = await PdfFile.find();
+        if (Pdffile) {
             return sendResponse(res, {
                 statusCode: httpStatus.OK,
                 success: true,
                 message: "Pdf files fetch successfully",
-                data:Pdffile
+                data: Pdffile
             });
-        }else{
-            throw new ApiError(404, "Files not found");   
+        } else {
+            throw new ApiError(404, "Files not found");
         }
-   
-      
-    }else{
+
+
+    } else {
         throw new ApiError(401, "You are unauthorized");
     }
 });
+
+
+
+exports.fileEdit = catchAsync(async (req, res, next) => {
+    const user = await User.findById(req.user._id);
+    if (user.role == "ADMIN" || "SUPER ADMIN") {
+
+        const formField = await Form.findById(req.params.id);
+        if (formField) {
+            const updatedData = req.body;
+            const formField = await Form.findByIdAndUpdate(req.params.id, updatedData, { new: true });
+            return sendResponse(res, {
+                statusCode: httpStatus.OK,
+                success: true,
+                message: "Form field updated successfully",
+                data: formField
+            });
+        }
+        else {
+            throw new ApiError(404, "Field not found");
+        }
+    } else {
+        throw new ApiError(401, "You are unauthorized");
+    }
+
+});
+
--- routes/form.route.js
+++ routes/form.route.js
@@ -6,6 +6,10 @@ const formController = require("../controllers/form.controller.js");
 
 router.post("/form-field-add",checkUser, formController.formFieldCreate);
 
+router.put("/form-field-edit/:id",checkUser, formController.fileEdit);
+
+router.get("/form-field-count", checkUser, formController.getFieldNumber);
+
 router.post("/pdf-save",checkUser,configureFileUpload(), formController.pdfSave);
 router.get("/all/pdf-fetch",checkUser,formController.allPdfFetch);
 
